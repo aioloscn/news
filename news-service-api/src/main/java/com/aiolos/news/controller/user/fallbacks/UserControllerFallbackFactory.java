93469e834b99d97f371d66cb24a33ad786a2f3cd
Aioloscn <756243011@qq.com>
2021-06-05 14:03:43 +0800

    Get user controller fallback cause

--- news-service-api/src/main/java/com/aiolos/news/controller/user/fallbacks/UserControllerFallbackFactory.java
+++ news-service-api/src/main/java/com/aiolos/news/controller/user/fallbacks/UserControllerFallbackFactory.java
@@ -5,14 +5,15 @@ import com.aiolos.news.common.enums.ErrorEnum;
 import com.aiolos.news.common.exception.CustomizedException;
 import com.aiolos.news.controller.user.UserControllerApi;
 import com.aiolos.news.pojo.bo.UpdateUserInfoBO;
-import com.aiolos.news.pojo.vo.UserBasicInfoVO;
 import feign.hystrix.FallbackFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 服务提供者熔断降级类，不写这个服务调用者会报500错误
@@ -26,30 +27,37 @@ public class UserControllerFallbackFactory implements FallbackFactory<UserContro
     @Override
     public UserControllerApi create(Throwable throwable) {
         return new UserControllerApi() {
+
+            private CommonResponse getCause() {
+                log.error(ErrorEnum.FEIGN_FALLBACK_EXCEPTION + ": " + throwable.getMessage());
+                Pattern pattern = Pattern.compile("(?<=\"msg\":\")(.*?)(?=\")");
+                Matcher matcher = pattern.matcher(throwable.getMessage());
+                String cause = StringUtils.EMPTY;
+                if (matcher.find()) {
+                    cause = matcher.group(0);
+                }
+                log.error("Connection refused, enter the degraded method of the service caller");
+                return CommonResponse.error(HttpStatus.INTERNAL_SERVER_ERROR.value(), cause);
+            }
+
             @Override
             public CommonResponse getUserBasicInfo(String userId) {
-                log.error("Connection refused, enter the degraded method of the service caller");
-                return CommonResponse.error(ErrorEnum.FEIGN_FALLBACK_EXCEPTION);
+                return getCause();
             }
 
             @Override
             public CommonResponse getAccountInfo(String userId) {
-                log.error("Connection refused, enter the degraded method of the service caller");
-                return CommonResponse.error(ErrorEnum.FEIGN_FALLBACK_EXCEPTION);
+                return getCause();
             }
 
             @Override
             public CommonResponse updateAccountInfo(@Valid UpdateUserInfoBO updateUserInfoBO) throws CustomizedException {
-                log.error("Connection refused, enter the degraded method of the service caller");
-                return CommonResponse.error(ErrorEnum.FEIGN_FALLBACK_EXCEPTION);
+                return getCause();
             }
 
             @Override
             public CommonResponse queryByIds(String userIds) {
-
-                log.error("Connection refused, enter the degraded method of the service caller");
-                List<UserBasicInfoVO> publisherList = new ArrayList<>();
-                return CommonResponse.ok(publisherList);
+                return getCause();
             }
         };
     }
